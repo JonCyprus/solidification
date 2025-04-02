edc13bb67f02441474377815987aa7452ff609e6
JonCyprus <joncyprus99@gmail.com>
2025-04-02 16:33:33 -0700

    started the upload functionality for interaction with database and s3 bucket

---
+++ go-tools/handlers/upload.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"errors"
+	"fmt"
+	"path/filepath"
+	cloudcfg "solidification/config_cloud"
+)
+
+func handlerUpload(cfg *cloudcfg.CloudConfig, args []string) error {
+	if len(args) != 2 {
+		return errors.New("usage: upload <one-body OR two-body> <filename.ext>")
+	}
+
+	// Check for proper arguments
+	if args[0] != "one-body" && args[0] != "two-body" {
+		return errors.New("usage: upload <one-body OR two-body> <filename.ext>")
+	}
+
+	ext := filepath.Ext(args[1])
+	if !validExts[ext] {
+		return fmt.Errorf("unsupported file extension: %s", ext)
+	}
+	filename := args[1]
+
+	// Update the sql record
+
+}
+
+var validExts = map[string]bool{
+	".jpg":  true,
+	".jpeg": true,
+	".mat":  true,
+}
