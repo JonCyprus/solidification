a84b3d53bcef121217aaa6421fe84d95d6c1a2cd
JonCyprus <joncyprus99@gmail.com>
2025-04-02 16:31:56 -0700

    updated initialization and .env load ins

--- go-tools/main.go
+++ go-tools/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"log"
 	"os"
 	cloudcfg "solidification/config_cloud"
 	"solidification/handlers"
@@ -14,6 +15,19 @@ func main() {
 	// Initialize config_cloud connections
 	cfg := cloudcfg.InitializeCloudConfig()
 	defer cfg.GetDB().Close(context.Background())
+	err := handlers.InvokeCommand("start-run", []string{}, cfg)
+	if err != nil {
+		log.Fatalf("error initializing unique runID: %v", err.Error())
+	}
+
+	// Current band-aid for setting the parameters, will change to be in dif config and change depending on run type
+	params, err := cloudcfg.UnmarshalRunParams()
+	if err != nil {
+		log.Fatalf("error unmarshalling params: %v", err.Error())
+	}
+	cfg.SetRunTemperature(params.Temperature)
+	cfg.SetRunDensity(params.Density)
+	cfg.SetRunVersion("Default")
 
 	// Start the REPL
 	fmt.Println("Simulation file uploader REPL ready. Type 'help' or 'exit'.")
@@ -37,7 +51,7 @@ func main() {
 		}
 
 		// Invoke the cmd from the input
-		err := handlers.InvokeCommand(cmd, args, cfg)
+		err = handlers.InvokeCommand(cmd, args, cfg)
 		if err != nil {
 			fmt.Println(err)
 		}
