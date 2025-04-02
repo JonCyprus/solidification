e8dd4dbf8ceff59a1bd401d334472fe33fe0eb36
JonCyprus <joncyprus99@gmail.com>
2025-04-02 16:42:36 -0700

    fixed parens mistake in CREATE TABLE statements

--- go-tools/sql/schema/001_param_filepath_tables.sql
+++ go-tools/sql/schema/001_param_filepath_tables.sql
@@ -1,25 +1,23 @@
-CREATE TABLE simulations.twobody_parameters (
-                                                    temperature double precision,
-                                                    density double precision,
-                                                    version text,
-                                                    runID uuid UNIQUE,
-                                                    note text,
-                                                    created_at timestamp NOT NULL,
-                                                    updated_at timestamp NOT NULL,
-                                                    CONSTRAINT twobody_param_composite_pk PRIMARY KEY (temperature, density, version)
-                                                );
-)
+CREATE TABLE twobody_parameters (
+                                    temperature double precision,
+                                    density double precision,
+                                    version text,
+                                    runID uuid UNIQUE,
+                                    note text,
+                                    created_at timestamp NOT NULL,
+                                    updated_at timestamp NOT NULL,
+                                    CONSTRAINT twobody_param_composite_pk PRIMARY KEY (temperature, density, version)
+                                );
 
-CREATE TABLE simulations.twobody_filepaths (
-                                                   runID uuid,
-                                                   category text,
-                                                   timestep bigint,
-                                                   filename text NOT NULL,
-                                                   created_at timestamp NOT NULL,
-                                                   updated_at timestamp NOT NULL,
-                                                   CONSTRAINT runID_fk FOREIGN KEY (runID)
-                                                       REFERENCES simulations.twobody_parameters (runID)
-                                                       ON DELETE CASCADE,
-                                                   CONSTRAINT twobody_files_composite_pk PRIMARY KEY (runID, category, timestep)
-                                               );
-)
+CREATE TABLE twobody_filepaths (
+                                   runID uuid,
+                                   category text,
+                                   timestep bigint,
+                                   filename text NOT NULL,
+                                   created_at timestamp NOT NULL,
+                                   updated_at timestamp NOT NULL,
+                                   CONSTRAINT runID_fk FOREIGN KEY (runID)
+                                       REFERENCES simulations.twobody_parameters (runID)
+                                       ON DELETE CASCADE,
+                                   CONSTRAINT twobody_files_composite_pk PRIMARY KEY (runID, category, timestep)
+                               );
