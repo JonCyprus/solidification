a46f39272651b7bff7274c178680508513f11be6
JonCyprus <joncyprus99@gmail.com>
2025-04-04 11:10:10 -0700

    lists all runs, needs support for flagging and eventually to distinguish between two-body and one-body runs

---
+++ go-tools/handlers/search/all_runs.go
@@ -0,0 +1,46 @@
+package search
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	cloudcfg "solidification/config_cloud"
+)
+
+// Include pagination later on to make it easier if you have a bajillion runs
+func searchAllRuns(cfg *cloudcfg.CloudConfig, args []string) error {
+	if len(args) < 1 {
+		return errors.New("usage: search <one-body OR two-body> -t [optional flag to show times]")
+	}
+
+	// Currently no flag support so just list without time
+	err := handleListAllTwoBodyRuns(cfg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func handleListAllTwoBodyRuns(cfg *cloudcfg.CloudConfig) error {
+	results, err := cfg.GetDBQueries().ListAllTwoBodyParams(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to fetch rows: %w", err)
+	}
+
+	if len(results) == 0 {
+		fmt.Println("No results found.")
+		return nil
+	}
+
+	for _, row := range results {
+		fmt.Printf("\nTemp: %.2f, Density: %.4f, Version: %s | RunID: %s\n",
+			row.Temperature, row.Density, row.Version, row.RunID)
+		//fmt.Println("---------------------------------------------------------------------------------------------")
+		for _ = range 150 {
+			fmt.Print("-")
+		}
+		fmt.Print("\n")
+	}
+
+	return nil
+}
